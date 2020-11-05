e6d0531e767ce2b9c1eb01041532f5e3fbcb95b4
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-11-05 13:30:23 -0500

    finished exception exercises

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -5,28 +5,67 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
+
         static double Divide(double x, double y)
         {
-            // Write your code here!
+            if (x != 0)
+            {
+                return x / y;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
+            }
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            if (fileName == null || fileName == "")
+            {
+                throw new ArgumentException();
+            }
+            else if (fileName.EndsWith(".cs"))
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
         }
 
 
-        static void Main(string[] args)
+    static void Main(string[] args)
         {
             // Test out your Divide() function here!
+            try
+            {
+                Console.WriteLine(Divide(0, 1));
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Possible points cannot be zero.");
+            }
+
 
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
             students.Add("Carl", "Program.cs");
             students.Add("Brad", "");
-            students.Add("Elizabeth", "MyCode.cs");
+            students.Add("Elizabeth", "MyCode.php");
             students.Add("Stefanie", "CoolProgram.cs");
 
+            foreach (KeyValuePair<string,string> student in students)
+            {
+                try
+                {
+                    Console.WriteLine(CheckFileExtension(student.Value));
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Students must submit a file for grading.");
+                }
+            }
 
         }
     }
